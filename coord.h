a2cbbd85116fd010a103c783cbee259ec7c3a9fb
Anton Voloshin <ashutosh108@gmail.com>
2019-03-14 10:47:18 +0200

    implement trivial automation for Ekadashi tests (to be improved)

--- coord.h
+++ coord.h
@@ -5,9 +5,9 @@
 struct Coord
 {
     double latitude, longitude;
-    explicit Coord(double _latitude, double _longitude)
+    explicit constexpr Coord(double _latitude, double _longitude)
         : latitude(_latitude), longitude(_longitude) {}
-    explicit Coord(int lat_deg, int lat_min, double lat_sec, int lng_deg, int lng_min, double lng_sec)
+    explicit constexpr Coord(int lat_deg, int lat_min, double lat_sec, int lng_deg, int lng_min, double lng_sec)
         : latitude(lat_deg + lat_min/60.0 + lat_sec/3600.0),
           longitude(lng_deg + lng_min/60.0 + lng_sec/3600.0) {}
 };
