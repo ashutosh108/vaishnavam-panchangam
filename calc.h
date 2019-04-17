5d3268f82265e9237d8278d707134410335a20b7
Anton Voloshin <ashutosh108@gmail.com>
2019-04-17 17:46:47 +0300

    Better comments for public Calc::... functions

--- calc.h
+++ calc.h
@@ -14,9 +14,18 @@ class Calc
 {
 public:
     Calc(Coord coord);
-    std::optional<Swe_Time> find_next_ekadashi_sunrise(Swe_Time after) const;
+    // main interface: get info for nearest future Vrata after given date
     std::optional<Vrata> find_next_vrata(Date after) const;
+
+    // Helper functions. They are public for easier testing,
+    // but should be considered private otherwise.
+
+    // find next sunrise which happens to be within Ekadashi tithi
+    std::optional<Swe_Time> find_next_ekadashi_sunrise(Swe_Time after) const;
+
+    // get arunodaya(= sunrise - night_length/8) for given sunrise
     std::optional<std::pair<Swe_Time, Swe_Time>> get_arunodaya(Swe_Time sunrise) const;
+
 private:
     Swe swe;
     std::optional<Swe_Time> get_prev_sunset(Swe_Time const sunrise) const;
