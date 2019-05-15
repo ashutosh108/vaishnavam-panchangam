a5c1814ade6865860c4ecedb3e8d4bb153d973f3
Anton Voloshin <ashutosh108@gmail.com>
2019-05-15 19:07:06 +0300

    get_next_tithi_start(): avoid endless loop.

--- calc.cpp
+++ calc.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <cassert>
 #include <cmath>
+#include <deque>
+#include <iostream>
 #include <optional>
 
 #include "calc.h"
@@ -119,17 +121,22 @@ std::optional<Swe_Time> Calc::get_next_tithi_start(Swe_Time from, Tithi tithi) c
     constexpr double average_tithi_length = 23.0/24 + 37.0/(24*60); // 23h37m
     Tithi cur_tithi = swe.get_tithi(from);
     double delta_tithi = cur_tithi.positive_delta_until_tithi(tithi);
-    double prev_delta_tithi = 0.0;
     Swe_Time time{from + delta_tithi * average_tithi_length};
     cur_tithi = swe.get_tithi(time);
+
+    double prev_abs_delta_tithi = std::numeric_limits<double>::max();
+
     while (cur_tithi != tithi) {
         delta_tithi = cur_tithi.delta_to_nearest_tithi(tithi);
-        // Break if delta no longer changes. It means we can't
-        // improve precision further, so break to return the best we have so far.
-        if (fabs(delta_tithi-prev_delta_tithi) < std::numeric_limits<double>::epsilon()) break;
-        prev_delta_tithi = delta_tithi;
         time += delta_tithi * average_tithi_length;
         cur_tithi = swe.get_tithi(time);
+
+        double const abs_delta_tithi = fabs(delta_tithi);
+        // Check for calculations loop: break if delta stopped decreasing (by absolute vbalue).
+        if (abs_delta_tithi >= prev_abs_delta_tithi) {
+            break;
+        }
+        prev_abs_delta_tithi = abs_delta_tithi;
     }
     return time;
 }
