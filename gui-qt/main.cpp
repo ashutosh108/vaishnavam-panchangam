ea2e146308ea2f6727d0300770b284695b96a5b3
Anton Voloshin <ashutosh108@gmail.com>
2020-02-03 08:59:09 +0200

    added initial Qt GUI
    other changes:
    - move text-interface to swe lib
    - gui-qt: use c++17
    - gui-qt: link with swe lib
    - gui-qt: add ../src as include dir
    - gui-qt: add universal exception handler (via QApplication::notify
    override)
    - gui-qt: change current dir to executable dir
    - gui-qt: set proper install_tz data dir
    - gui-qt: update result text area with calculations result in
    on_FindNextEkadashi_clicked()

--- gui-qt/main.cpp
+++ gui-qt/main.cpp
@@ -1,10 +1,35 @@
+#include <filesystem>
+#include <iostream>
+#include <QApplication>
+#include <QDir>
+#include <QMessageBox>
+
+#include "tz-fixed.h"
 #include "mainwindow.h"
 
-#include <QApplication>
+
+class MyApplication : public QApplication {
+public:
+    MyApplication(int& argc, char** argv) :
+      QApplication(argc, argv) {}
+    bool notify(QObject* receiver, QEvent* event) override {
+        bool done = true;
+        try {
+            done = QApplication::notify(receiver, event);
+        } catch (const std::exception &e) {
+            QMessageBox::warning(nullptr, "fatal error", e.what());
+        }
+        return done;
+    }
+};
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    MyApplication a(argc, argv);
+    auto curdir = QCoreApplication::applicationDirPath();
+    QDir::setCurrent(curdir + "/..");
+    std::cerr << "current_path: " << std::filesystem::current_path() << '\n';
+    date::set_install("tzdata");
     MainWindow w;
     w.show();
     return a.exec();
--- gui-qt/mainwindow.cpp
+++ gui-qt/mainwindow.cpp
@@ -1,6 +1,11 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QMessageBox>
+#include <sstream>
+
+#include "text-interface.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +18,19 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_FindNextEkadashi_clicked()
+{
+    auto date_string = ui->dateLineEdit->text();
+    QByteArray date_as_bytearray = date_string.toLocal8Bit();
+    auto base_date = vp::text_ui::parse_ymd(date_as_bytearray.data());
+    auto location_string = ui->LocationLineEdit->text();
+    QByteArray location_as_bytearray = location_string.toLocal8Bit();
+    char * location_as_cstr = location_as_bytearray.data();
+
+    std::stringstream s;
+    vp::text_ui::calc_one(
+                base_date, location_as_cstr, s);
+
+    ui->calcResult->setText(QString::fromStdString(s.str()));
+}
