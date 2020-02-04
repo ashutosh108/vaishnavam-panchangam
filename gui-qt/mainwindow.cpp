f37af1dab086cefb408a4a3a9021848da50322c8
Anton Voloshin <ashutosh108@gmail.com>
2020-02-04 22:17:01 +0200

    ensure we never pass exceptions from on_button_click

--- gui-qt/mainwindow.cpp
+++ gui-qt/mainwindow.cpp
@@ -21,16 +21,23 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_FindNextEkadashi_clicked()
 {
-    auto date_string = ui->dateLineEdit->text();
-    QByteArray date_as_bytearray = date_string.toLocal8Bit();
-    auto base_date = vp::text_ui::parse_ymd(date_as_bytearray.data());
-    auto location_string = ui->LocationLineEdit->text();
-    QByteArray location_as_bytearray = location_string.toLocal8Bit();
-    char * location_as_cstr = location_as_bytearray.data();
+    try {
+        auto date_string = ui->dateLineEdit->text();
+        QByteArray date_as_bytearray = date_string.toLocal8Bit();
+        auto base_date = vp::text_ui::parse_ymd(date_as_bytearray.data());
 
-    std::stringstream s;
-    vp::text_ui::calc_one(
-                base_date, location_as_cstr, s);
+        auto location_string = ui->LocationLineEdit->text();
+        QByteArray location_as_bytearray = location_string.toLocal8Bit();
+        char * location_as_cstr = location_as_bytearray.data();
 
-    ui->calcResult->setText(QString::fromStdString(s.str()));
+        std::stringstream s;
+        vp::text_ui::calc_one(
+                    base_date, location_as_cstr, s);
+
+        ui->calcResult->setText(QString::fromStdString(s.str()));
+    } catch (std::exception &e) {
+        QMessageBox::warning(this, "error", e.what());
+    } catch (...) {
+        QMessageBox::warning(this, "internal error", "unexpected exception thrown");
+    }
 }
