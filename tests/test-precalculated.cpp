be66eae049c54903ecba0bb162d896a63c2d5fbb
Anton Voloshin <ashutosh108@gmail.com>
2020-02-13 17:23:24 +0200

    WIP precalc-test: implement get_ekadashi_name()

--- tests/test-precalculated.cpp
+++ tests/test-precalculated.cpp
@@ -1,11 +1,13 @@
 
 #include "html-parser.h"
+#include "vrata.h"
 
 #include "catch.hpp"
 #include "date-fixed.h"
 #include <filesystem>
 #include <fstream>
 #include <map>
+#include <regex>
 #include <sstream>
 
 std::string slurp_file(std::filesystem::path filename) {
@@ -73,19 +75,54 @@ col_to_date get_date_headers(html::Table & t, date::year year) {
     return map;
 }
 
-/* detect whether known ekAdashI is described in this cell.
+/* In: text from cell in pañcāṅgam, possibly including "such-and-such ekAdashI" in Russian language. E.g. "Варӯтӿинӣ экāдащӣ"
+ * Detect whether known ekAdashI is described in this cell.
  * fail check on unknown ekAdashI name.
  * Return empty string if ekAdashI is not mentioned at all.
  * Return ekAdashI name otherwise;
  */
-std::string detect_ekadashi([[maybe_unused]] const std::string & text) {
-    // TODO: implement
-    return "";
+std::string get_ekadashi_name(const std::string & text) {
+    std::regex regex{"([^\\s,.]+)[ -](э|Э)кāдащӣ"};
+    std::smatch match;
+    if (!std::regex_search(text, match, regex)) {
+        return "";
+    }
+    std::string ekadashi_name = match[1].str();
+    // require ekadashi_name to be known
+    REQUIRE_THAT(ekadashi_name,
+                 Catch::Predicate<const std::string &>(
+                     [](const std::string & s) -> bool {
+                        return std::find(vp::ekadashi_names_rus().begin(), vp::ekadashi_names_rus().end(), s) != vp::ekadashi_names_rus().end();
+                     }
+    ));
+    return ekadashi_name;
+}
+
+TEST_CASE("get_ekadashi_name works") {
+    REQUIRE(get_ekadashi_name("Варӯтӿинӣ экāдащӣ") == "Варӯтӿинӣ");
+    REQUIRE(get_ekadashi_name(", Варӯтӿинӣ экāдащӣ, ") == "Варӯтӿинӣ");
 }
 
 // TODO: remove [[maybe_unused]] after completing this check.
 size_t check_ekadashi([[maybe_unused]] const std::vector<std::string> & row, [[maybe_unused]] std::size_t col, [[maybe_unused]] const std::string & ekadashi_name) {
     return 0;
+}
+
+std::string join(const std::vector<std::string> & v, char joiner=';') {
+    std::string joined;
+    bool first = true;
+    for (const auto & cell : v) {
+        if (!first) {
+            joined += joiner;
+        }
+        first = false;
+        joined += cell;
+    }
+    return joined;
+}
+
+TEST_CASE("join() works") {
+    REQUIRE("a;b" == join(std::vector<std::string>{"a", "b"}));
 }
 
 /* If indicated cell text represents vrata we know how to handle (ekAdashI), then check if our calculations give the same result
@@ -94,26 +131,34 @@ size_t check_ekadashi([[maybe_unused]] const std::vector<std::string> & row, [[m
  * Two for normal case of single-day ekAdashI (vrata+pAraNam)
  * Zero if we didn't detect any vrata we would know how to handle.
  */
-std::size_t check_vrata(const std::vector<std::string> & row, std::size_t col) {
-    std::string ekadashi_name = detect_ekadashi(row[col]);
+std::size_t check_vrata(const std::string & case_slug, const std::vector<std::string> & row, std::size_t col) {
+    std::string ekadashi_name = get_ekadashi_name(row[col]);
     if (ekadashi_name.empty()) return 0;
-    // first +1 to compensate zero-based counting
-    // second +1 means we require that there is at least one more column.
+    // first +1 is to compensate zero-based counting
+    // second +1 means we require that there is at least one more column (for pAraNam).
     REQUIRE(row.size() >= col+1+1);
     std::size_t cells_handled = check_ekadashi(row, col, ekadashi_name);
-    REQUIRE(cells_handled >= 2);
+    if (cells_handled < 2) {
+        std::string row_str = join(row);
+        REQUIRE_THAT(cells_handled,
+                     Catch::Predicate<std::size_t>([](std::size_t cells_handled) -> bool {
+                         return cells_handled >= 2;
+                     },
+                     case_slug + ": can't handle ekAdashI at col " + std::to_string(col) + " of " + row_str
+                     ));
+    }
     return cells_handled;
 }
 
 /* Returns number of vratas handled in this row (should always be 1 for I haven't seen two ekAdashI vratas in a single table yet)
  */
-std::size_t check_vratas_from_row(const std::vector<std::string> & row, const col_to_date & date_map) {
+std::size_t check_vratas_from_row(const std::string & case_slug, const std::vector<std::string> & row, const col_to_date & date_map) {
     // manual loop because occasionally we need to increment the iterator manually to skip some cells
     std::size_t vratas_handled = 0;
     for (auto iter = date_map.begin(); iter != date_map.end(); ++iter) {
         auto & col = iter->first;
 
-        std::size_t handled_cells = check_vrata(row, col);
+        std::size_t handled_cells = check_vrata(case_slug, row, col);
         vratas_handled += (handled_cells > 0);
         // TODO: when handled_cells > 1, check if all correponding next dates in date_map are always one day after a previous one,
         // fail the test otherwise.
@@ -146,7 +191,7 @@ void test_one_precalculated_case(const std::string & slug) {
 
         // TODO: check that timezone in the table matches our data
 
-        std::size_t ekadashi_etc_count = check_vratas_from_row(row_data, date_headers);
+        std::size_t ekadashi_etc_count = check_vratas_from_row(slug, row_data, date_headers);
         std::string row_str;
         bool first = true;
         for (const auto & cell : row_data) {
