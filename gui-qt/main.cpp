039cf8fa7f5450546a703b9673d22afe17a03a75
Anton Voloshin <ashutosh108@gmail.com>
2020-03-05 09:50:33 +0200

    make "about" window text copyable

--- gui-qt/main.cpp
+++ gui-qt/main.cpp
@@ -21,12 +21,16 @@ public:
         }
         return done;
     }
+    void make_all_qmessagebox_texts_selectable() {
+        setStyleSheet("QMessageBox { messagebox-text-interaction-flags: 5; }");
+    }
 };
 
 int main(int argc, char *argv[])
 {
     vp::text_ui::change_to_data_dir(argv[0]);
     MyApplication a(argc, argv);
+    a.make_all_qmessagebox_texts_selectable();
     date::set_install("tzdata");
     MainWindow w;
     w.show();
