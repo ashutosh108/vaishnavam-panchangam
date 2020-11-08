d821a92e22f1e7d43be1a91ffcfd2c0eaf1ee856
Anton Voloshin <ashutosh108@gmail.com>
2020-11-08 11:51:08 +0200

    add CATCH_CONFIG_ENABLE_ALL_STRINGMAKERS to handle std::chrono::duration etc

--- tests/catch-formatters.h
+++ tests/catch-formatters.h
@@ -3,6 +3,7 @@
 
 #include "juldays_ut.h"
 
+#define CATCH_CONFIG_ENABLE_ALL_STRINGMAKERS
 #include "catch.hpp"
 #include <optional>
 
@@ -11,18 +12,6 @@ template<>
 struct StringMaker<vp::JulDays_UT> {
     static std::string convert(const vp::JulDays_UT & t) {
         return fmt::to_string(t);
-    }
-};
-}
-
-namespace Catch {
-template<>
-struct StringMaker<std::optional<vp::JulDays_UT>> {
-    static std::string convert(const std::optional<vp::JulDays_UT> & t) {
-        if (t) {
-            return fmt::to_string(*t);
-        }
-        return "[nullopt]";
     }
 };
 }
