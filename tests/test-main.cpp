57b18326559e55c052c2150a3f9861550c1040e3
Anton Voloshin <ashutosh108@gmail.com>
2020-12-26 10:38:19 +0200

    test-main: print overall run duration

--- tests/test-main.cpp
+++ tests/test-main.cpp
@@ -5,14 +5,21 @@
 #include <Windows.h>
 #endif
 
+#include "fmt/chrono.h"
 #include "tz-fixed.h"
 
 int main(int argc, char* argv[]) {
+    auto start = std::chrono::high_resolution_clock::now();
 
 #ifdef _WIN32
     SetConsoleOutputCP(CP_UTF8);
 #endif
 
     date::set_install("tzdata");
-    return Catch::Session().run(argc, argv);
+    auto ret_code = Catch::Session().run(argc, argv);
+
+    std::chrono::duration<double> duration = std::chrono::high_resolution_clock::now() - start;
+    fmt::print(FMT_STRING("Run time: {}\n"), duration);
+
+    return ret_code;
 }
