01763ab3a7360b1903c8df7ab36274266f832818
Anton Voloshin <ashutosh108@gmail.com>
2021-02-24 19:01:39 +0200

    add madhyahna-, aparāhna- and sāyaṁ-kāla to day-by-day.

--- src/text-interface.cpp
+++ src/text-interface.cpp
@@ -406,7 +406,10 @@ DayByDayInfo daybyday_events(date::year_month_day base_date, const vp::Calc & ca
         if (sunset) {
             info.sunset1 = *sunset;
             info.events.push_back(NamedTimePoint{"sunset", *sunset});
-            info.events.push_back(NamedTimePoint{"1/5 of daytime", calc.proportional_time(*sunrise, *sunset, 0.2)});
+            info.events.push_back(NamedTimePoint{"1/5 of daytime (saṁgava-kāla begins)", calc.proportional_time(*sunrise, *sunset, 0.2)});
+            info.events.push_back(NamedTimePoint{"2/5 of daytime (madhyahna-kāla begins)", calc.proportional_time(*sunrise, *sunset, 0.4)});
+            info.events.push_back(NamedTimePoint{"3/5 of daytime (aparāhna-kāla begins)", calc.proportional_time(*sunrise, *sunset, 0.6)});
+            info.events.push_back(NamedTimePoint{"4/5 of daytime (sāyam-kāla begins)", calc.proportional_time(*sunrise, *sunset, 0.8)});
             info.events.push_back(NamedTimePoint{"middle of the day", calc.proportional_time(*sunrise, *sunset, 0.5)});
             const auto sunrise2 = calc.swe.find_sunrise(*sunset);
             if (sunrise2) {
