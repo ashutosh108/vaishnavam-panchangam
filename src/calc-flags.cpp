7ac34a9480d325205ed9c265a491537d9b2dc4e4
Anton Voloshin <ashutosh108@gmail.com>
2021-02-26 08:54:27 +0200

    add CalcFlags::ShravanaDvadashi{12,14}ghPlus

--- src/calc-flags.cpp
+++ src/calc-flags.cpp
@@ -2,9 +2,10 @@
 
 #include <type_traits>
 
+using underlying = std::underlying_type_t<vp::CalcFlags>;
+
 vp::CalcFlags vp::operator&(vp::CalcFlags lhs, vp::CalcFlags rhs)
 {
-    using underlying = std::underlying_type_t<vp::CalcFlags>;
     return static_cast<vp::CalcFlags>(
         static_cast<underlying>(lhs) &
         static_cast<underlying>(rhs));
@@ -12,7 +13,6 @@ vp::CalcFlags vp::operator&(vp::CalcFlags lhs, vp::CalcFlags rhs)
 
 vp::CalcFlags vp::operator|(vp::CalcFlags lhs, vp::CalcFlags rhs)
 {
-    using underlying = std::underlying_type_t<vp::CalcFlags>;
     return static_cast<vp::CalcFlags>(
         static_cast<underlying>(lhs) |
         static_cast<underlying>(rhs));
--- src/calc-flags.h
+++ src/calc-flags.h
@@ -1,6 +1,7 @@
 #ifndef CALC_FLAGS_H
 #define CALC_FLAGS_H
 
+#include <type_traits>
 
 namespace vp {
 
@@ -18,6 +19,9 @@ enum class CalcFlags {
     RiseSetGeocentricMask = 8,
     RiseSetGeocentricOff = 0, // default: do not set GEOCTR_NO_ECL_LAT
     RiseSetGeocentricOn = 8, // set GEOCTR_NO_ECL_LAT for rise-set-related calls
+    ShravanaDvadashiMask = 16,      // Be default, Śravaṇa-nakṣatra must be present on Dvādaśī for 12+ ghaṭikas
+    ShravanaDvadashi12ghPlus = 0,   // after sunrise for Śravaṇa-dvādaśī (i.e. enter Madhyāhna-kāla at least briefly)
+    ShravanaDvadashi14ghPlus = 16,  // OR, require 14+ ghaṭikas (i.e. Śr-nakṣatra must not only enter Madhyahna-kāla, but also enter it's middle ghaṭika, 15th ghaṭika from suryodaya)
     Default = 0, // Default must be zero because we are ORing it with flags sometimes
 };
 
