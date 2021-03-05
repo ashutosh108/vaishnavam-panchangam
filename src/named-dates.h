da4e9195528a550bffbffe4617fde646423c8b24
Anton Voloshin <ashutosh108@gmail.com>
2021-03-05 14:20:06 +0200

    add NamedDate constructor with default param values for cleaner init

--- src/named-dates.h
+++ src/named-dates.h
@@ -10,6 +10,8 @@ struct NamedDate {
     std::string name;
     std::string title;
     std::string css_classes;
+    NamedDate(std::string name_, std::string title_="", std::string css_classes_="") :
+          name(name_), title(title_), css_classes(css_classes_) {}
 };
 
 using NamedDates = std::multimap<date::local_days, NamedDate>;
